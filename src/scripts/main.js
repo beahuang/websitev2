9447dca607ba64d85360631ed0e11b43a6523579
Beatrice Huang <beahuang1@gmail.com>
2015-09-27 12:38:02 +1000

    add in base styles and main page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
 /** @jsx React.DOM */
 var React = require('react');
 var App = require('./views.jsx');
-React.render(<App/>, document.getElementById('container'));
+React.render(<App/>, document.getElementById('devPanel'));
